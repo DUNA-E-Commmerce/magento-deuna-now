d1f229bbcccfff13bf31baa601222465de19a3c9
Engelbertg J Bracho R <3ng3l64@gmail.com>
2023-09-04 19:17:05 -0400

    refactor(PLAT-1167): Refactor de payment methods eliminado custom code

--- Model/ClearCartAndRedirect.php
+++ Model/ClearCartAndRedirect.php
@@ -2,15 +2,23 @@
 namespace Deuna\Now\Model;
 
 use Magento\Checkout\Model\Session as CheckoutSession;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Message\ManagerInterface;
 
 class ClearCartAndRedirect
 {
     protected $checkoutSession;
+    protected $messageManager;
+    protected $context;
 
     public function __construct(
         CheckoutSession $checkoutSession,
+        ManagerInterface $messageManager,
+        Context $context
     ) {
         $this->checkoutSession = $checkoutSession;
+        $this->messageManager = $messageManager;
+        $this->context = $context;
     }
 
     /**
@@ -22,6 +30,11 @@ class ClearCartAndRedirect
     {
         try {
             $this->checkoutSession->clearQuote();
+            $this->messageManager->addSuccessMessage(__('Tu compra se ha realizado con éxito.'));
+
+            $resultRedirect = $this->context->getResultRedirectFactory()->create();
+            $resultRedirect->setPath('checkout/onepage/success'); 
+
             return true;
         } catch (\Exception $e) {
             return false;
